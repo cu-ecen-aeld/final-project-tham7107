8cc285f9d3498cec7f3d75951a063a3eb3ead4e9
Thomas Ames <tham7107@colorado.edu>
2024-07-22 12:38:06 -0600

    RPi 3B+ support

--- runqemu.sh
+++ runqemu.sh
@@ -4,12 +4,42 @@
 #Author: Siddhant Jajoo.
 
 
-qemu-system-aarch64 \
-    -M virt  \
-    -cpu cortex-a53 -nographic -smp 1 \
-    -kernel buildroot/output/images/Image \
-    -append "rootwait root=/dev/vda console=ttyAMA0" \
-    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
-    -device virtio-net-device,netdev=eth0 \
-    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
-    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+# original command
+#qemu-system-aarch64 \
+#    -M virt  \
+#    -cpu cortex-a53 -nographic -smp 1 \
+#    -kernel buildroot/output/images/Image \
+#    -append "rootwait root=/dev/vda console=ttyAMA0" \
+#    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
+#    -device virtio-net-device,netdev=eth0 \
+#    -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
+#    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+
+# Ubuntu 20.04 (version spec'ed for ECEA530x) comes with QEMU
+# version 4.2.1, which has limited RPi emulation. I manually
+# downloaded and built 9.0.1 to get better RPi emulation.
+QEMU=qemu/build/qemu-system-aarch64
+# Default is ctrl-a (0x01), which interferes with bash line editing.
+# 0x1c is ctrl-\ (FS, "file separator")
+QEMU_ESC_CHAR=0x1c
+
+# DTB from build
+# DTB=output/images/bcm2710-rpi-3-b-plus.dtb
+# Bookworm default Pi 3B+ DTB with BT disabled and ttyAMA0 added
+DTB=/home/tga/ecea530x/ecea5307/bcm2710-rpi-3-b-plus-no-bt-w-uart0.dtb
+KERNEL_CMD_LINE="rootwait root=/dev/mmcblk0p2 console=ttyAMA0"
+
+case "$1" in
+    bookworm)
+	KERNEL=kernel8.img
+	IMG=2024-03-15-raspios-bookworm-arm64-lite.img
+	;;
+    *)
+	KERNEL=buildroot/output/images/Image
+	IMG=buildroot/output/images/sdcard.img
+    ;;
+esac
+
+echo $QEMU -echr $QEMU_ESC_CHAR -M raspi3b -m 1G -kernel $KERNEL -dtb $DTB -append \"$KERNEL_CMD_LINE\" -drive file=$IMG,if=sd,format=raw -device usb-net,netdev=usbnet0 -netdev user,id=usbnet0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 -nographic
+
+$QEMU -echr $QEMU_ESC_CHAR -M raspi3b -m 1G -kernel $KERNEL -dtb $DTB -append "$KERNEL_CMD_LINE" -drive file=$IMG,if=sd,format=raw -device usb-net,netdev=usbnet0 -netdev user,id=usbnet0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 -nographic
