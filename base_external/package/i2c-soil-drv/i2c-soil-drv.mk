a055569c07944da9971623cf8b2d5e853ec8350c
Thomas Ames <tham7107@colorado.edu>
2024-07-28 15:02:19 -0600

    Update git hash for issue 3 commit

--- base_external/package/i2c-soil-drv/i2c-soil-drv.mk
+++ base_external/package/i2c-soil-drv/i2c-soil-drv.mk
@@ -3,7 +3,8 @@
 #
 
 # a07c04c8976dbd766774b303c2f7ab858f5299a2 is skel commit on issue3 branch
-I2C_SOIL_DRV_VERSION = a07c04c8976dbd766774b303c2f7ab858f5299a2
+# 480506cadecfc906ae8a2acafac6965553fe2fec is issue3 commit on main (merge back issue 3 branch
+I2C_SOIL_DRV_VERSION = 480506cadecfc906ae8a2acafac6965553fe2fec
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -12,13 +13,10 @@ I2C_SOIL_DRV_SITE = git@github.com:tham7107/soil-monitor.git
 I2C_SOIL_DRV_SITE_METHOD = git
 I2C_SOIL_DRV_GIT_SUBMODULES = YES
 
-#I2C_SOIL_DRV_VERSION = 1.0
-#I2C_SOIL_DRV_SITE = $(BR2_EXTERNAL_SOIL_DRIVER_PATH)
-#I2C_SOIL_DRV_SITE_METHOD = local
-
 # No quotes around list - won't build, silently fails
 I2C_SOIL_DRV_MODULE_SUBDIRS = i2c-soil-drv
 
+# Copy driver test script to /root on target
 define I2C_SOIL_DRV_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/i2c-soil-drv/i2c-soil-drv-test.sh $(TARGET_DIR)/root
 endef
